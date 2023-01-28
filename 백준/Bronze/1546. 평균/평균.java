5cb7c2014848fd2154323d31224e742f6e6e37d5
taekto <110243496+taekto@users.noreply.github.com>
2023-01-28 23:56:33 +0900

    [Bronze I] Title: 평균, Time: 352 ms, Memory: 24032 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 과목의 갯수 N개 값 받기
+		int N = sc.nextInt();
+		// N개의 자리를 가진 배열 만들기
+		float[] arr = new float[N];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextFloat();
+		}
+		
+		// max값 탐색
+		float max = arr[0];
+		for(int i=0;i<arr.length;i++) {
+			if(max<arr[i]) {
+				max = arr[i];
+			}
+		}
+		
+		float result = 0;
+		for(int i=0;i<arr.length;i++) {
+			result += (arr[i]/max)*100;
+		}
+		float avg = result/arr.length;
+		System.out.println(avg);
+}
+}
