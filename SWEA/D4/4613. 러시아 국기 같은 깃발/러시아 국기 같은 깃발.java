38e1742c5d8379c33634a24ba84c8901df598f33
taekto <110243496+taekto@users.noreply.github.com>
2023-03-03 10:20:35 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 182 ms, Memory: 25,696 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -1,68 +1,100 @@
 import java.util.Scanner;
 
 public class Solution {
+	static char[][] map;
+	static int N, M, min;
+	static char[] colors; // 재귀적으로 선택된 행별 색깔을 담을 배열
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int T = sc.nextInt();
+		
 		for(int tc=1;tc<=T;tc++) {
-			int N = sc.nextInt(); // 행의 수
-			int M = sc.nextInt(); // 열의 수
+			N = sc.nextInt();
+			M = sc.nextInt();
+			
+			map = new char[N][M];
+			colors = new char[N]; // 행의 정보만 가지면 되므로
 			
 			// 2차원 배열 입력받기
-			char[][] map = new char[N][M];
 			for(int r=0;r<N;r++) {
-				String str = sc.next(); // r번째 행의 문자열을 한 행씩 받아오기
-				char[] arr = str.toCharArray(); // 문자의 배열로 쪼개기
+				String str = sc.next();
+				char[] arr = str.toCharArray();
 				for(int c=0;c<M;c++) {
 					map[r][c] = arr[c];
 				}
 			}
 			
-			// 최소값을 구해야 하므로.. 최대값으로 초기화 하고 시작
-			int min = Integer.MAX_VALUE;
+			min = Integer.MAX_VALUE; // 최소값 초기화
 			
-			// b : 파란색(B)가 시작되는 idx
-			// r : 빨간색(R)이 시작되는 idx
+			// select 재귀적으로 행의 색깔을 선택하는 함수
+			// 각 선택은 이전 색깔의 영향을 받음
+			// select(i): i번째 행의 색을 선택 (x)
+			// i번째 행의 선택은 i-1번째 행의 색깔에 의존, 영향을 받음
+			// select(i, prevColor) : i-1번째 색이 prevColor일 때, i번째 색을 선택
+			select(1, 'W'); // 1번부터 N-2번까지 선택
 			
-			// i : 행의 idx
-			// j : 2차원 배열의 열의 idx
+			// 재귀함수가 다 돌고나면 1번행부터 N-2번행 까지 최소값이 min에 저장되어 있어요..
 			
-			// 완전탐색
-			for(int b=1;b<N-1;b++) {
-				for(int r=2;r<N;r++) {
-					// b, r이 정해질 때마다
-					// 새 구간마다 카운트를 해야 함.
-					int cnt = 0;
-					// [0, b)
-					for(int i=0;i<b;i++) {
-						for(int j=0;j<M;j++) {
-							if(map[i][j] != 'W') cnt++;
-						}
-					}
-					// [b, r)
-					for(int i=b;i<r;i++) { // 참일때만 반복되므로 
-						for(int j=0;j<M;j++) {
-							if(map[i][j] != 'B') cnt++;
-						}
-					}
-					// [r, N)
-					for(int i=r;i<N;i++) {
-						for(int j=0;j<M;j++) {
-							if(map[i][j] != 'R') cnt++;
-						}
-					}
-					
-					// 다른 것의 개수가 다 세어진 상황
-					// 최소값인지 판단하고 갱신
-					if(cnt < min) min = cnt;
-					
+			for(int j=0;j<M;j++) {
+				// 0번 행은 어차피 W
+				if(map[0][j] != 'W') {
+					min++;
+				}
+				// 1번 ~ N-2번까지만 선택하면 됨
+				if(map[N-1][j] != 'R') {
+					min++;
 				}
 			}
-			
+			// N-1번 행은 어차피 R
 			System.out.println("#"+tc+" "+min);
-			
-			
 		}
+	}
+
+	// i-1번째 색이 주어질 때 i번째 색을 선택하는 함수
+	// 변수명을 고칠 때 : Alt + Shift + R
+	private static void select(int idx, char c) {
+		// 기저 조건
+		if(idx == N-1) { // N-2까지는 선택을 하고, N-1일 때 멈춤.
+			// 선택이 완료되었으므로 개수를 셈
+			int colorCnt = 0;
+			
+			for(int i=1;i<N-1;i++) {
+				int cnt = 0;
+				for(int j=0;j<M;j++) {
+					if(map[i][j] != colors[i]) cnt++;
+				}
+				colorCnt += cnt;
+			}
+			
+			min = (min > colorCnt) ? colorCnt : min;
+			
+			return;
+		}
+		
+		// i가 N-2번째를 선택하려고 하는데..
+		// N-3번째 색이 'W' 였으면.. => N-2번째는 무조건 'B'가 되도록 선택
+		if(idx == N-2 && c == 'W') {
+			colors[idx] = 'B';
+			select(idx+1, 'B');
+			return;
+		}
+		
+		// 유도 조건
+		if(c == 'W') {
+			colors[idx] = 'W';
+			select(idx+1, 'W');
+			colors[idx] = 'B';
+			select(idx+1, 'B');
+		} else if(c == 'B') {
+			colors[idx] = 'B';
+			select(idx+1, 'B');
+			colors[idx] = 'R';
+			select(idx+1, 'R');
+		} else if(c == 'R') {
+			colors[idx] = 'R';
+			select(idx+1, 'R');
+		}
+		
 	}
 }
