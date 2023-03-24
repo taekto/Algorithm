5eb27af4eecdf98e6dd3be9c2d5ac794615bc66c
taekto <110243496+taekto@users.noreply.github.com>
2023-03-24 11:44:03 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 20584 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static int N, M;
+	public static int[] arr;
+	public static boolean[] visited;
+	public static int[] temp;
+	public static StringBuilder sb = new StringBuilder();
+
+	
+	public static void dfs(int count, int idx) {
+		if(count == M) {
+//			System.out.println(Arrays.toString(temp));
+			for(int i=0;i<temp.length;i++) {
+				sb.append(temp[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=idx;i<N;i++) {
+			temp[count] = arr[i]; 
+			dfs(count+1,i);
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		visited = new boolean[N];
+		temp = new int[M];
+		for(int i=0;i<N;i++) {
+			arr[i] = i+1;
+		}
+		
+//		System.out.println(Arrays.toString(arr));
+		dfs(0, 0);
+		System.out.println(sb);
+		
+	}
+}
