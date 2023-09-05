8d0ddd17b4d96daffe95e90a7c41e59fb28e9800
taekto <110243496+taekto@users.noreply.github.com>
2023-09-05 19:34:25 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 232 ms, Memory: 42020 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,76 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static class Node {
-		int y;
-		int x;
-		
-		public Node(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-
-		public int getY() {
-			return y;
-		}
-
-		public void setY(int y) {
-			this.y = y;
-		}
-
-		public int getX() {
-			return x;
-		}
-
-		public void setX(int x) {
-			this.x = x;
-		}
-		
-	}
-	
 	static char[][] arr;
 	static boolean[][] visited;
-	
 	static int N, M;
-	
+	static int count = 0;
 	static int[] dy = {-1, 1, 0, 0};
 	static int[] dx = {0, 0, -1, 1};
 	
-	static int count = 0;
-  	
-	static void bfs(int y, int x) {
-		Queue<Node> q = new LinkedList<>();
-		visited[y][x] = true;
-		q.add(new Node(y, x));
-
+	public static void dfs(int y, int x) {
 		
-		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			y = cur.getY();
-			x = cur.getX();
-			
-			for(int i=0;i<4;i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-				if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-				else if(visited[ny][nx] == false && arr[ny][nx] == 'O') {
-					visited[ny][nx] = true;
-					q.add(new Node(ny,nx));
-				}
-				else if(visited[ny][nx] == false && arr[ny][nx] == 'P') {
-					visited[ny][nx] = true;
-					q.add(new Node(ny,nx));
-					count++;
-				}
+		visited[y][x] = true;
+		
+		if(arr[y][x] == 'P') {
+			count++;
+		}
+		
+		for(int i=0;i<4;i++) {
+			int ny = dy[i] + y;
+			int nx = dx[i] + x;
+			if(ny >= 0 && ny < N && nx >= 0 && nx < M && arr[ny][nx] != 'X' && visited[ny][nx] == false) {
+				dfs(ny, nx);
 			}
-			
-			
 		}
 		
 	}
@@ -85,28 +40,27 @@ public class Main {
 		
 		arr = new char[N][M];
 		visited = new boolean[N][M];
-		int starty = 0;
-		int startx = 0;
+		int y = 0;
+		int x = 0;
 		
 		for(int i=0;i<N;i++) {
 			String str = br.readLine();
 			for(int j=0;j<M;j++) {
-				char a = str.charAt(j);
-				if(a == 'I') {
-					starty = i;
-					startx = j;
+				if(str.charAt(j) == 'I') {
+					y = i;
+					x = j;
 				}
-				arr[i][j] = a;
+				arr[i][j] = str.charAt(j);
 			}
 		}
 		
-		bfs(starty, startx);
+		dfs(y, x);
 		if(count == 0) {
 			System.out.println("TT");
-		} else {
+		}
+		else {
 			System.out.println(count);
 		}
-		
 		
 	}
 }
