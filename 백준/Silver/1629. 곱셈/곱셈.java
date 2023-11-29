cea44e45d33658ed4cab5e31ddc6eb771ed584c3
taekto <110243496+taekto@users.noreply.github.com>
2023-11-29 15:34:57 +0900

    [Silver I] Title: 곱셈, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -4,21 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long POW(long a, long b, long m) {
-        if (b == 1) return a % m;
-        long val = POW(a, b / 2, m);
-        val = val * val % m;
-        if (b % 2 == 0) return val;
-        return val * a % m;
+
+    static long recursive(long a, long b, long c) {
+        if(b == 1) return a%c;
+        long val = recursive(a,b/2,c);
+        val = val*val%c;
+        if(b%2 == 0) return val;
+        return val*a%c;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         long a = Long.parseLong(st.nextToken());
         long b = Long.parseLong(st.nextToken());
         long c = Long.parseLong(st.nextToken());
-        System.out.println(POW(a,b,c));
+
+        System.out.println(recursive(a,b,c));
+
     }
 }
