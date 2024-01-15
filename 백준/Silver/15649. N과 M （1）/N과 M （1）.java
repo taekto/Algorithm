486edc35df328dcd661ee342f82ce05eee62d69e
taekto <110243496+taekto@users.noreply.github.com>
2024-01-15 16:24:42 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 20248 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,44 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static int N, M;
-	public static boolean[] visited;
-	public static int[] arr;
-	
-	public static void dfs(int count, int idx) {
-		if(count == M) {
-			for(int i=0;i<M;i++) {
-				System.out.print(arr[i]+" ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i=0;i<N;i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				arr[idx] = i+1;
-				dfs(count+1, idx+1);
-				visited[i] = false;
-				arr[idx] = 0;
-			}
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		arr = new int[M];
-		visited = new boolean[N];
-		dfs(0,0);
-	}
+
+    static int N, M;
+    static int[] arr = new int[10];
+    static boolean[] visited = new boolean[10];
+    static StringBuilder sb = new StringBuilder();
+
+    static void recursive(int k) {
+        if (k == M) {
+            for(int i=0;i<M;i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        for(int i=1;i<=N;i++) {
+            if(!visited[i]) {
+                arr[k] = i;
+                visited[i] = true;
+                recursive(k+1);
+                visited[i] = false;
+            }
+        }
+
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        recursive(0);
+        System.out.println(sb.toString());
+
+    }
 }
