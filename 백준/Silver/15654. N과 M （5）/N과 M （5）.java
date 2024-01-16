09d422f046f29185f88e1e8a971700f6e8177602
taekto <110243496+taekto@users.noreply.github.com>
2024-01-16 16:15:58 +0900

    [Silver III] Title: N과 M (5), Time: 260 ms, Memory: 30980 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static boolean[] visited = new boolean[10];
+    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr = new int[10];
+
+    static void func(int k) {
+        if(k == M) {
+            for(int i=0;i<M;i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[k] = list.get(i);
+                func(k+1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+        func(0);
+        System.out.println(sb.toString());
+
+    }
+}
