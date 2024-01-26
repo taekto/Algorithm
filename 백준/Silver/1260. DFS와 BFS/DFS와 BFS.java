aadeb11de79286fa51319bfcc154f0a3b4a02f83
taekto <110243496+taekto@users.noreply.github.com>
2024-01-26 10:53:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 18132 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,74 +1,71 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	public static List<List<Integer>> list = new ArrayList<>();
-	public static int N, M, V;
-	public static boolean[] visited;
-	
-	public static void dfs(int start) {
-		
-		visited[start] = true;
-		System.out.print(start+" ");
-		for(int i=0;i<list.get(start).size();i++) {
-			if(!visited[list.get(start).get(i)]) {
-				visited[list.get(start).get(i)] = true;
-				dfs(list.get(start).get(i));
-			}
-		}
-	}
-	
-	public static void bfs(int start) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(start);
-		visited[start] = true;
-		while(!q.isEmpty()) {
-			int node = q.poll();
-			System.out.print(node+" ");
-			if(!list.get(node).isEmpty()) {
-				for(int i=0;i<list.get(node).size();i++) {
-					int num = list.get(node).get(i);
-					if(!visited[num]) {
-						visited[num] = true;
-						q.add(num);
-					}
-				}
-			}
-		}
-		System.out.println();
-	}
-	
-	
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		V = sc.nextInt();
-		visited = new boolean[N+1];
-		for(int i=0;i<=N;i++) {
-			List<Integer> temp = new ArrayList<>();
-			list.add(temp);
-		}
-		for(int i=0;i<M;i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			list.get(a).add(b);
-			list.get(b).add(a);
-		}
-		for(int i=0;i<=N;i++) {
-			list.get(i).sort(null);
-		}
-		
-		
-		dfs(V);
-		visited = new boolean[N+1];
-		System.out.println();
-		bfs(V);
-	}
+
+    static int N,M,V;
+    static List<Integer>[] list;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void bfs(int k) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(k);
+        visited[k] = true;
+        sb.append(k).append(" ");
+        while(!q.isEmpty()) {
+            k = q.poll();
+            for(int i=0;i<list[k].size();i++) {
+                if(visited[list[k].get(i)]) continue;
+                visited[list[k].get(i)] = true;
+                sb.append(list[k].get(i)).append(" ");
+                q.add(list[k].get(i));
+            }
+        }
+    }
+
+    static void recursive(int k) {
+        visited[k] = true;
+        sb.append(k).append(" ");
+        for(int i=0;i<list[k].size();i++) {
+            if(visited[list[k].get(i)]) continue;
+            recursive(list[k].get(i));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        list = new ArrayList[N+1];
+        for(int i=1;i<=N;i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i=0;i<M;i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list[u].add(v);
+            list[v].add(u);
+        }
+
+        for(int i=1;i<=N;i++) {
+            Collections.sort(list[i]);
+        }
+
+        visited = new boolean[N+1];
+        recursive(V);
+        sb.append("\n");
+        visited = new boolean[N+1];
+        bfs(V);
+
+        System.out.println(sb.toString());
+
+    }
 }
