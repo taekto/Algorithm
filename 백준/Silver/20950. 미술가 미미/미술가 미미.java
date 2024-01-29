a8d361721cd226018b4b290bb0a64a75b55f42e1
taekto <110243496+taekto@users.noreply.github.com>
2024-01-29 14:13:21 +0900

    [Silver II] Title: 미술가 미미, Time: 148 ms, Memory: 12096 KB -BaekjoonHub

---
+++ 백준/Silver/20950. 미술가 미미/미술가 미미.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] R;
+    static int[] G;
+    static int[] B;
+    static int[] arr;
+    static int min = Integer.MAX_VALUE;
+
+
+    static void func(int target,int depth, int idx,int r,int g,int b) {
+        if(depth == target) {
+            r = r/depth;
+            g = g/depth;
+            b = b/depth;
+            min = Math.min(min,Math.abs(r-arr[0])+Math.abs(g-arr[1])+Math.abs(b-arr[2]));
+            return;
+        }
+
+        if(idx == N) return;
+
+        func(target,depth+1, idx+1, r+R[idx],g+G[idx],b+B[idx]);
+        func(target,depth,idx+1,r,g,b);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        R = new int[N];
+        G = new int[N];
+        B = new int[N];
+
+        for(int i=0;i<N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            R[i] = Integer.parseInt(st.nextToken());
+            G[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        arr = new int[3];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+        int stop = 0;
+        if(N <= 7)  stop = N;
+        else stop = 7;
+
+        for(int i=2;i<=stop;i++) {
+            func(i,0,0,0,0,0);
+        }
+        System.out.println(min);
+
+    }
+}
