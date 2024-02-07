e2174f2a6aba2353a57871aaf0daff475d822d70
taekto <110243496+taekto@users.noreply.github.com>
2024-02-07 12:45:01 +0900

    [Silver IV] Title: 로프, Time: 376 ms, Memory: 26432 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<N;i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        int ans = 0;
+        for(int i=0;i<N;i++) {
+            int weight = list.get(i);
+            ans = Math.max(weight*(N-i), ans);
+        }
+        System.out.println(ans);
+    }
+}
