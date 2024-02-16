f6a6b3d0b14b602008d1f4afc709cc82a65fcd9a
taekto <110243496+taekto@users.noreply.github.com>
2024-02-16 10:30:52 +0900

    [Silver IV] Title: 국영수, Time: 1844 ms, Memory: 76220 KB -BaekjoonHub

--- 백준/Silver IV/10825. 국영수/국영수.java
+++ 백준/Silver IV/10825. 국영수/국영수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.BatchUpdateException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,14 +32,10 @@ public class Main {
                     if(this.math<o.math) return 1;
                     else if(this.math>o.math) return -1;
                     else {
-                        for(int i=0;i<Math.min(this.name.length(),o.name.length());i++) {
-                            if(this.name.charAt(i) == o.name.charAt(i)) continue;
-                            return (this.name.charAt(i)-o.name.charAt(i));
-                        }
+                        return this.name.compareTo(o.name);
                     }
                 }
             }
-            return 0;
         }
     }
 
