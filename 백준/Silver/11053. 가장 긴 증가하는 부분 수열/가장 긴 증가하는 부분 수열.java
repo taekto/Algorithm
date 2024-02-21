fb33d852fc1217ff12b120b0c8c3b72892e63801
taekto <110243496+taekto@users.noreply.github.com>
2024-02-21 11:21:45 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,31 +5,38 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N;
+    static int[] arr;
+    static int[] num;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[1001];
-        int[] num = new int[1001];
-
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        num = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         Arrays.fill(num, 1);
-
         for(int i=1;i<=N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         for(int i=2;i<=N;i++) {
-            for(int j=1;j<i;j++) {
+            for(int j=1;j<=i-1;j++) {
                 if(arr[i] > arr[j]) {
-                    num[i] = Math.max(num[i], num[j]+1);
+                    if(num[j]+1 > num[i]) {
+                        num[i] = num[j]+1;
+                    }
                 }
             }
         }
+
         int result = 0;
+
         for(int i=1;i<=N;i++) {
             result = Math.max(result, num[i]);
         }
+
         System.out.println(result);
+
     }
 }
