b25902283026ccd32bc8b481e1e84fae8de4fe38
taekto <110243496+taekto@users.noreply.github.com>
2024-03-11 10:59:38 +0900

    [Silver II] Title: 이동하기, Time: 388 ms, Memory: 76908 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N+1][M+1];
+        for(int i=1;i<=N;i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=M;j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=1;i<=N;i++) {
+            for(int j=1;j<=M;j++) {
+                map[i][j] = Math.max(map[i-1][j-1], Math.max(map[i-1][j], map[i][j-1]))+map[i][j];
+            }
+        }
+
+        System.out.println(map[N][M]);
+
+    }
+}
