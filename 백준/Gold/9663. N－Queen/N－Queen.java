2d1f39c7c0372b66b6a40209f07d4b64e9f78c3a
taekto <110243496+taekto@users.noreply.github.com>
2024-05-12 19:13:26 +0900

    [Gold IV] Title: N-Queen, Time: 2576 ms, Memory: 12216 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,66 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static int N;
-	public static int[] arr;
-	public static boolean[] visited1 = new boolean[40];
-	public static boolean[] visited2 = new boolean[40];
-	public static boolean[] visited3 = new boolean[40];
-	public static int result = 0;
-	
-	// 이거는 왜 안됨?
-//	public static void dfs(int count) {
-//		if(count == N) {
-//			result++;
-//			return;
-//		}
-//		
-//		for(int i=0;i<N;i++) {
-//			if(visited1[count] || visited2[i+count] || visited3[i-count+N-1]) {
-//				continue;
-//			}
-//			visited1[count] = true;
-//			visited2[i+count] = true;
-//			visited3[i-count+N-1] = true;
-//			dfs(count+1);
-//			visited1[count] = false;
-//			visited2[i+count] = false;
-//			visited3[i-count+N-1] = false;
-//		}
-//	}
-	
-	 // 이거는 왜 되고
-	public static void dfs(int count) {
-		if(count == N) {
-			result++;
-			return;
-		}
-		
-		for(int i=0;i<N;i++) {
-			if(visited1[i] || visited2[i+count] || visited3[count-i+N-1]) {
-				continue;
-			}
-			visited1[i] = true;
-			visited2[i+count] = true;
-			visited3[count-i+N-1] = true;
-			dfs(count+1);
-			visited1[i] = false;
-			visited2[i+count] = false;
-			visited3[count-i+N-1] = false;
-		}
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		
-		dfs(0);
-		System.out.println(result);
-		
-		
-		
-	}
+
+    static int N;
+    static boolean[] visited1;
+    static boolean[] visited2;
+    static boolean[] visited3;
+    static int ans = 0;
+    static void func(int depth) {
+        if(depth == N) {
+            ans++;
+            return;
+        }
+
+        for(int i=0;i<N;i++) {
+            if(!visited1[i] && !visited2[depth-i+N-1] && !visited3[depth+i]) {
+                visited1[i] = true;
+                visited2[depth-i+N-1] = true;
+                visited3[depth+i] = true;
+                func(depth+1);
+                visited1[i] = false;
+                visited2[depth-i+N-1] = false;
+                visited3[depth+i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        visited1 = new boolean[N];
+        visited2 = new boolean[2*N-1];
+        visited3 = new boolean[2*N-1];
+        func(0);
+        System.out.println(ans);
+    }
 }
